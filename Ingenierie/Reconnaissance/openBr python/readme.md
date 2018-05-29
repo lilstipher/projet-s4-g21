9bf342dc1720f9739c77f663f65450480230b396
Henri <sirmichel1995@gmail.com>
2018-05-29 20:27:30 +0200

    We can see the accuracy

---
+++ Ingenierie/Reconnaissance/openBr python/readme.md
@@ -0,0 +1,19 @@
+Autors: Lilstipher
+# openBr with Python
+## how to use this code ?
+### 1. Training
+```
+$ python train.py
+```
+### 2. Generate score matrix
+```
+$ python test.py
+```
+### 3. Show results (recall , accuracy)
+```
+$ python result.py
+```
+then ...
+
+
+
--- Ingenierie/Reconnaissance/openBr python/result.py
+++ Ingenierie/Reconnaissance/openBr python/result.py
@@ -1,4 +1,7 @@
 import csv
+from PIL import Image
+from os import listdir,walk,path
+from os.path import isfile, join
 scorefile= open('scores.csv', 'rb') 
 reader =csv.reader(scorefile)
 print(reader)
@@ -13,7 +16,28 @@ for x in scorelist[0]:
     if len(y)>2:
         y=y[len(y)-2]
     header.append(y)
+
+count=0
+nbr=0
 for x in scorelist[1:]:
-    print(x[0],header[x.index(max(x))],max(x))
+    nbr+=1
+    y=header[x.index(max(x))]
+    z=header[x.index(min(x))]
+    a,b=float(max(x)),float(min(x))
+    #print(float(a))
+    if abs(a) > abs(b):
+        y=y
+    else :
+        y=z
+    print(x[0],y)#,max(x))
+    if y in x[0]:
+        count+=1
+mypath="test"
+imageFiles = [ join(mypath,f) for f in listdir(mypath) if isfile(join(mypath,f)) and (f.endswith("G") or f.endswith("g")) ]
+numberFace=len(imageFiles)
+print("accuracy:",float(count)/nbr)
+print("recall:",float(count)/numberFace)
+print(count)
+#print(scorelist[1:2])
 
 
